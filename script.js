097035c5edace69e2183ac8c9c985976bdc29e15
sharvankhajuria <sharvankhajuria1@gmail.com>
2026-07-05 15:12:32 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+window.addEventListener("scroll", function () {
+
+const navbar = document.querySelector(".navbar");
+
+if(window.scrollY > 50){
+
+navbar.classList.add("scrolled");
+
+}
+else{
+
+navbar.classList.remove("scrolled");
+
+}
+
+});
